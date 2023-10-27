ce9134af53c3899db241a9d60047b1089bfb292c
Santiago Rivera Loaiza <32771909+SRIVERAL@users.noreply.github.com>
2023-10-27 14:21:33 -0500

    Chnage message v2

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 let d = new Date();
 
-document.body.innerHTML = "<h1>Hello World..!! The time right now is:  " + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds() + "</h1>"
+document.body.innerHTML = "<h1>Hello Felipe..!! The time right now is:  " + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds() + "</h1>"
